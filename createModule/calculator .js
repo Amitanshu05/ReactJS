1061ca6cb93e603a391ad188c99733ec90de6f7d
Amitanshu Varshney <amitanshu.varshney@gmail.com>
2025-05-30 14:14:00 +0530

    learnt about creating modules and their properties

---
+++ createModule/calculator .js
@@ -0,0 +1,15 @@
+let sumData = (a,b)=>{
+   return a+b ; 
+}
+
+export {sumData};
+
+
+let reduceData = (a,b)=>{
+   return a-b ; 
+}
+
+export {reduceData};
+
+
+export let cname = "KRSNA";
---
+++ createModule/index.js
@@ -0,0 +1,8 @@
+import { cname } from "./calculator ";
+import {sumData} from "./calculator .js ";
+import { reduceData } from "./calculator .js ";
+
+
+console.log(sumData(211,11));
+console.log(reduceData(211,11));
+console.log(cname);
